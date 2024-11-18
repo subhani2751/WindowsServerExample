3f1b30e897d722351ac035914e0bf5261e49afd5
subhani2751 <subhanishaik2751999@gmail.com>
2024-11-18 08:13:11 +0530

    first commte

---
+++ WindowsServerExample/BackgroudTimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf.Builders;
+
+namespace WindowsServerExample
+{
+    public class BackgroudTimer
+    {
+        private readonly Timer _timer;
+        public BackgroudTimer()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += timerElapsed;
+
+        }
+        private void timerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] line = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Windows\Temp\BackgroudTimer.txt", line);
+        }
+        public void Start() 
+        {
+            _timer.Start();
+        }
+        public void Stop() 
+        { 
+            _timer.Stop();
+        }
+    }
+}
---
+++ WindowsServerExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WindowsServerExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostconfigration => {
+                hostconfigration.Service<BackgroudTimer>(s => {
+                    s.ConstructUsing(backgroudTimer => new BackgroudTimer());
+                    s.WhenStarted(backgroudTimer => backgroudTimer.Start());
+                    s.WhenStopped(backgroudTimer => backgroudTimer.Stop());
+                });
+                hostconfigration.RunAsLocalSystem();
+                hostconfigration.SetServiceName("BackGroudservices");
+                hostconfigration.SetDisplayName("Back Groud services");
+                hostconfigration.SetDescription("to run background tasks");
+            });
+
+        }
+    }
+}
